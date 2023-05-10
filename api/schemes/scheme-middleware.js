8b619095b14aad6db1ca1f1f7c5eceffad3b6f9a
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-10 11:35:09 -0700

    middleware fleshed out

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -20,7 +20,7 @@ const checkSchemeId = async (req, res, next) => {
         message: `scheme with scheme_id ${req.params.scheme_id} not found`,
       });
     } else {
-      next()
+      next();
     }
   } catch (error) {
     next(error);
@@ -35,7 +35,21 @@ const checkSchemeId = async (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {};
+const validateScheme = (req, res, next) => {
+  const { scheme_name } = req.body;
+  if (
+    !scheme_name ||
+    typeof scheme_name !== "string" ||
+    scheme_name.trim() !== ""
+  ) {
+    next({
+      status: 400,
+      message: "invalid scheme_name",
+    });
+  } else {
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -46,7 +60,25 @@ const validateScheme = (req, res, next) => {};
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {};
+const validateStep = (req, res, next) => {
+  const { instructions, step_number } = req.body;
+  if (
+    !instructions ||
+    typeof instructions !== "string" ||
+    instructions.trim() == "" ||
+    !step_number ||
+    typeof step_number !== "number" ||
+    step_number == isNaN ||
+    step_number < 1
+  ) {
+    next({
+      status: 400,
+      message: `invalid step`,
+    });
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   checkSchemeId,
